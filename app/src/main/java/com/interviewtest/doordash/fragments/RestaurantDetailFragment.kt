32e2f63361d522d3d1537807f4f9b3a4c8603a91
Oleg Zilberman <olegzilberman.zilberman@gmail.com>
2019-01-02 10:50:59 -0800

    fix to save state

--- app/src/main/java/com/interviewtest/doordash/fragments/RestaurantDetailFragment.kt
+++ app/src/main/java/com/interviewtest/doordash/fragments/RestaurantDetailFragment.kt
@@ -1,16 +1,12 @@
 package com.interviewtest.doordash.fragments
 
 import android.os.Bundle
-import com.interviewtest.doordash.datamodel.DisplayedPageState
 import com.interviewtest.doordash.datamodel.FragmentSaveState
-import com.interviewtest.doordash.recyclerviewadapters.GenericImageAdapter
 import com.interviewtest.doordash.server.NetworkServiceInitializer
 import com.interviewtest.doordash.server.RetrofitNetworkService
-import com.interviewtest.doordash.utilities.populateRestaurantData
-import io.reactivex.Single
 import kotlinx.serialization.json.JSON
 
-class RestaurantListFragment : BaseFragment() {
+class RestaurantDetailFragment : BaseFragment() {
     private lateinit var serverCall: RetrofitNetworkService
     private lateinit var initialLocation: Pair<Float, Float>
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,37 +31,27 @@ class RestaurantListFragment : BaseFragment() {
         }
     }
 
-    override fun fetchServerData(): Single<DisplayedPageState>? {
-        return serverCall.getApi()?.run {
-            val result: Single<String>? =
-                fetchRestaurantList(initialLocation.first, initialLocation.second, GenericImageAdapter.maxAdapterSize)
-            result?.run {
-                flatMap { items ->
-                    populateRestaurantData(items)?.let { itemList ->
-                        Single.just(DisplayedPageState(0, 0, itemList))
-                    } ?: let {
-                        Single.error<DisplayedPageState>(Throwable("Empty Payload"))
-                    }
-                }
-            }
-        }
-    }
-
     companion object {
-        const val fragmentID = "5a5e870f-01db-4e44-977e-0212501177a5"
-        @JvmStatic
-        fun newInstance(): RestaurantListFragment {
+        const val fragmentID = "5df86723-6e72-4ea1-9a68-c0c119723521"
+        fun newInstance(): RestaurantDetailFragment {
             val bundle = Bundle().apply {
                 val state =
-                    FragmentSaveState(fragmentID, "https://api.doordash.com/v2/restaurant/", 37.422740f, -122.139956f, 2)
+                    FragmentSaveState(
+                        RestaurantListFragment.fragmentID,
+                        "https://api.doordash.com/v2/restaurant/",
+                        37.422740f,
+                        -122.139956f,
+                        2
+                    )
                 putSerializable(
                     BaseFragment.BASE_FRAGMENT_INITIAL_DATA_KEY,
                     JSON.stringify(FragmentSaveState.serializer(), state)
                 )
             }
-            val retVal = RestaurantListFragment()
-            retVal.arguments = bundle
-            return retVal
+
+            val fragment = RestaurantDetailFragment()
+            fragment.arguments = bundle
+            return fragment
         }
     }
 }
